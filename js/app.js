e19c209a4886ac67663ca7c2fa62e90e25f524ed
Shrnivas Kopparam Ramanath <shrini@uw.edu>
2014-11-01 17:58:32 -0700

    Fixed and working game

--- js/app.js
+++ js/app.js
@@ -2,12 +2,13 @@
 "use strict";
 
 var tiles = [];
-var tile;
-var turn = false;
+//var tile;
+var totalPairs;
 var remaining;
 var missed;
-var matched;
 var previousImg;
+var detectClick;
+var gameTimer;
 for(var i = 1; i < 32; i++) {
     tiles.push({
         tileNum: i,
@@ -22,13 +23,14 @@ console.log(tiles);
 //when document is ready...
 $(document).ready(function(){
     //catch click event of start game button
+    detectClick = true
     $('#start-game').click(function(){
         console.log('start game button clicked!');
         tiles = _.shuffle(tiles);
         var selectedTiles = tiles.slice(0,8);
-        missed = 0;
-        var detectClick = true;
-        remaining = selectedTiles.size;
+        totalPairs = selectedTiles.length;
+        resetGame();
+        updateStatistics();
         var tilePairs = [];
         _.forEach(selectedTiles, function(tile) {
             tilePairs.push(tile);
@@ -57,11 +59,12 @@ $(document).ready(function(){
 
         //get starting milliseconds
         var startTime = Date.now();
-        window.setInterval(function() {
+        gameTimer = window.setInterval(function() {
             var elapsedSeconds = (Date.now() - startTime) / 1000;
             elapsedSeconds = Math.floor(elapsedSeconds);
             $('#elapsed-seconds').text(elapsedSeconds + ' seconds');
         }, 1000);
+
 
         $('#game-board img').click(function() {
             if(!detectClick) {
@@ -79,26 +82,40 @@ $(document).ready(function(){
                 } else {
                     var previousTile = previousImg.data('tile');
                     if(previousTile.src == tile.src) {
-                        remaining-= 1;
+                        remaining -= 1;
+                        $('#matches-missed').text('Matches missed: ' + missed);
                         previousImg = null;
                     } else {
                         detectClick = false;
                         window.setTimeout(function () {
                             flipTile(tile, clickedIMG);
                             flipTile(previousTile, previousImg);
-                            missed+= 1;
+                            missed += 1;
                             $('#matches-missed').text('Matches missed: ' + missed);
                             previousImg = null;
                             detectClick = true;
                         }, 1000);
                     }
                 }
+                updateStatistics();
             }
         })
-
     }); //start game button click
 }); //document ready button
+function updateStatistics() {
+    $('#wrong-matches').text(" " + missed);
+    $('#matches-left').text(" " + remaining);
+    $('#matches-made').text(" " + (totalPairs - remaining));
+}
 
+function resetGame() {
+    $('#game-board').empty();
+    remaining = totalPairs;
+    missed = 0;
+    detectClick = true;
+    previousImg = null;
+    window.clearInterval(gameTimer);
+}
 function flipTile(tile, img) {
     //if
     img.fadeOut(100, function() {
